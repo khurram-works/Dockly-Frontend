e413f0d7e268b5b3bdc5e195b5e220465ba05efa
khurram-works <rajpootkhurramx82@gmail.com>
2026-06-23 18:40:54 +0500

    Resolved few errors

--- api/auth.ts
+++ api/auth.ts
@@ -1,5 +1,5 @@
 import { apiRequest, publicApiRequest, documents } from "./client";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 interface loginType {
   email: string;
